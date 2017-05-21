c637d39d738dcab64c94cace56bc06b44d76d116
lucaa <lucaazalim@gmail.com>
2017-05-21 01:40:33 -0300

    Is it now a builder? Maybe I'll change it soon.

--- src/main/java/ch/jamiete/mcping/MinecraftPingOptions.java
+++ src/main/java/ch/jamiete/mcping/MinecraftPingOptions.java
@@ -28,50 +28,21 @@
  */
 package ch.jamiete.mcping;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
 /**
  * Storage class for {@link MinecraftPing} options.
  */
+@Getter
+@Setter
+@Accessors(fluent = true)
 public class MinecraftPingOptions {
 
     private String hostname;
     private int port = 25565;
     private int timeout = 2000;
     private String charset = "UTF-8";
-
-    public MinecraftPingOptions setHostname(String hostname) {
-        this.hostname = hostname;
-        return this;
-    }
-
-    public MinecraftPingOptions setPort(int port) {
-        this.port = port;
-        return this;
-    }
-
-    public MinecraftPingOptions setTimeout(int timeout) {
-        this.timeout = timeout;
-        return this;
-    }
-
-    public MinecraftPingOptions setCharset(String charset) {
-        this.charset = charset;
-        return this;
-    }
-
-    public String getHostname() {
-        return this.hostname;
-    }
-
-    public int getPort() {
-        return this.port;
-    }
-
-    public int getTimeout() {
-        return this.timeout;
-    }
-
-    public String getCharset() {
-        return this.charset;
-    }
 
 }
