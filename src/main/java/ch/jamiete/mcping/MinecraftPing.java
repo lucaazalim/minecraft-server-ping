a432b99db358fafc50d6eaeff060c0aad6b9181f
lucaa <lucaazalim@gmail.com>
2017-05-21 01:01:03 -0300

    Using try-with-resources on MinecraftPing.

--- src/main/java/ch/jamiete/mcping/MinecraftPing.java
+++ src/main/java/ch/jamiete/mcping/MinecraftPing.java
@@ -63,62 +63,64 @@ public class MinecraftPing {
         Preconditions.checkNotNull(options.getHostname(), "Hostname cannot be null.");
         Preconditions.checkNotNull(options.getPort(), "Hostname cannot be null.");
 
-        final Socket socket = new Socket();
-        socket.connect(new InetSocketAddress(options.getHostname(), options.getPort()), options.getTimeout());
+        String json;
 
-        final DataInputStream in = new DataInputStream(socket.getInputStream());
-        final DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+        try (final Socket socket = new Socket()) {
 
-        //> Handshake
-        ByteArrayOutputStream handshake_bytes = new ByteArrayOutputStream();
-        DataOutputStream handshake = new DataOutputStream(handshake_bytes);
+            socket.connect(new InetSocketAddress(options.getHostname(), options.getPort()), options.getTimeout());
 
-        handshake.writeByte(MinecraftPingUtil.PACKET_HANDSHAKE);
-        MinecraftPingUtil.writeVarInt(handshake, MinecraftPingUtil.PROTOCOL_VERSION);
-        MinecraftPingUtil.writeVarInt(handshake, options.getHostname().length());
-        handshake.writeBytes(options.getHostname());
-        handshake.writeShort(options.getPort());
-        MinecraftPingUtil.writeVarInt(handshake, MinecraftPingUtil.STATUS_HANDSHAKE);
+            try (DataInputStream in = new DataInputStream(socket.getInputStream())) {
+                try (DataOutputStream out = new DataOutputStream(socket.getOutputStream())) {
 
-        MinecraftPingUtil.writeVarInt(out, handshake_bytes.size());
-        out.write(handshake_bytes.toByteArray());
+                    //> Handshake
+                    try (ByteArrayOutputStream handshake_bytes = new ByteArrayOutputStream()) {
+                        try (DataOutputStream handshake = new DataOutputStream(handshake_bytes)) {
 
-        //> Status request
-        out.writeByte(0x01); // Size of packet
-        out.writeByte(MinecraftPingUtil.PACKET_STATUSREQUEST);
+                            handshake.writeByte(MinecraftPingUtil.PACKET_HANDSHAKE);
+                            MinecraftPingUtil.writeVarInt(handshake, MinecraftPingUtil.PROTOCOL_VERSION);
+                            MinecraftPingUtil.writeVarInt(handshake, options.getHostname().length());
+                            handshake.writeBytes(options.getHostname());
+                            handshake.writeShort(options.getPort());
+                            MinecraftPingUtil.writeVarInt(handshake, MinecraftPingUtil.STATUS_HANDSHAKE);
 
-        //< Status response
-        MinecraftPingUtil.readVarInt(in); // Size
-        int id = MinecraftPingUtil.readVarInt(in);
+                            MinecraftPingUtil.writeVarInt(out, handshake_bytes.size());
+                            out.write(handshake_bytes.toByteArray());
 
-        MinecraftPingUtil.io(id == -1, "Server prematurely ended stream.");
-        MinecraftPingUtil.io(id != MinecraftPingUtil.PACKET_STATUSREQUEST, "Server returned invalid packet.");
+                            //> Status request
+                            out.writeByte(0x01); // Size of packet
+                            out.writeByte(MinecraftPingUtil.PACKET_STATUSREQUEST);
 
-        int length = MinecraftPingUtil.readVarInt(in);
-        MinecraftPingUtil.io(length == -1, "Server prematurely ended stream.");
-        MinecraftPingUtil.io(length == 0, "Server returned unexpected value.");
+                            //< Status response
+                            MinecraftPingUtil.readVarInt(in); // Size
+                            int id = MinecraftPingUtil.readVarInt(in);
 
-        byte[] data = new byte[length];
-        in.readFully(data);
-        String json = new String(data, options.getCharset());
+                            MinecraftPingUtil.io(id == -1, "Server prematurely ended stream.");
+                            MinecraftPingUtil.io(id != MinecraftPingUtil.PACKET_STATUSREQUEST, "Server returned invalid packet.");
 
-        //> Ping
-        out.writeByte(0x09); // Size of packet
-        out.writeByte(MinecraftPingUtil.PACKET_PING);
-        out.writeLong(System.currentTimeMillis());
+                            int length = MinecraftPingUtil.readVarInt(in);
+                            MinecraftPingUtil.io(length == -1, "Server prematurely ended stream.");
+                            MinecraftPingUtil.io(length == 0, "Server returned unexpected value.");
 
-        //< Ping
-        MinecraftPingUtil.readVarInt(in); // Size
-        id = MinecraftPingUtil.readVarInt(in);
-        MinecraftPingUtil.io(id == -1, "Server prematurely ended stream.");
-        MinecraftPingUtil.io(id != MinecraftPingUtil.PACKET_PING, "Server returned invalid packet.");
+                            byte[] data = new byte[length];
+                            in.readFully(data);
+                            json = new String(data, options.getCharset());
 
-        // Close
-        handshake.close();
-        handshake_bytes.close();
-        out.close();
-        in.close();
-        socket.close();
+                            //> Ping
+                            out.writeByte(0x09); // Size of packet
+                            out.writeByte(MinecraftPingUtil.PACKET_PING);
+                            out.writeLong(System.currentTimeMillis());
+
+                            //< Ping
+                            MinecraftPingUtil.readVarInt(in); // Size
+                            id = MinecraftPingUtil.readVarInt(in);
+                            MinecraftPingUtil.io(id == -1, "Server prematurely ended stream.");
+                            MinecraftPingUtil.io(id != MinecraftPingUtil.PACKET_PING, "Server returned invalid packet.");
+
+                        }
+                    }
+                }
+            }
+        }
 
         return new Gson().fromJson(json, MinecraftPingReply.class);
     }
