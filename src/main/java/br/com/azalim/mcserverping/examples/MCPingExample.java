45a45134fda5944650f6993b80a83ad91ab36e2d
Luca Azalim <lucaazalim@gmail.com>
2021-02-24 20:46:38 -0300

    Update server IP used in example class.

--- src/main/java/br/com/azalim/mcserverping/examples/MCPingExample.java
+++ src/main/java/br/com/azalim/mcserverping/examples/MCPingExample.java
@@ -16,7 +16,7 @@ public class MCPingExample {
     public static void main(String[] args) {
 
         MCPingOptions options = MCPingOptions.builder()
-                .hostname("skyminigames.com.br")
+                .hostname("redesky.com")
                 .build();
 
         MCPingResponse reply;
